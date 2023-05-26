5f35ee4cebd3fe2b4203cb712a305d13b74b5b65
Jonas Østergaard <jonasoe@hotmail.com>
2023-05-26 10:29:22 +0200

    first commit

---
+++ js/Modules/filter.js
@@ -0,0 +1,21 @@
+const filterIgnore = ['homeworld', 'films', 'species', 'vehicles', 'starships', 'created', 'edited', 'url',
+ 'residents', 'characters', 'planets', 'people', 'pilots'];
+export {filterIgnore} 
+
+
+// Name: Luke Skywalker
+// Height: 172
+// Mass: 77
+// Hair Color: Blond
+// Skin Color: Fair
+// Eye Color: Blue
+// Birth Year: 19BBY
+// Gender: Male
+// Homeworld: Https://Swapi.Dev/Api/Planets/1/
+// Films: Https://Swapi.Dev/Api/Films/1/,Https://Swapi.Dev/Api/Films/2/,Https://Swapi.Dev/Api/Films/3/,Https://Swapi.Dev/Api/Films/6/
+// Species:
+// Vehicles: Https://Swapi.Dev/Api/Vehicles/14/,Https://Swapi.Dev/Api/Vehicles/30/
+// Starships: Https://Swapi.Dev/Api/Starships/12/,Https://Swapi.Dev/Api/Starships/22/
+// Created: 2014-12-09T13:50:51.644000Z
+// Edited: 2014-12-20T21:17:56.891000Z
+// Url: Https://Swapi.Dev/Api/People/1/
